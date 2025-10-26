428e70e09236eb04a33733093209a4b717a214d2
Devendra Kumar <devendra8182@gmail.com>
2025-10-26 21:36:04 +0530

    Bug fixed in backup script

--- Assignment_SDLC_testing_Linux-server/Mike_nginx_backup.sh
+++ Assignment_SDLC_testing_Linux-server/Mike_nginx_backup.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
-DATE = $(date +%y%m%d)
-BACKUP_DIR = "/backups"
-BACKUP_FILE = "$BACKUP_DIR/Mike_nginx_backup_$DATE.tar.gz"
-LOG_FILE = "$BACKUP_DIR/Mike_nginx_backup_log_$DATE.txt"
+DATE=$(date +%y%m%d)
+BACKUP_DIR="/backups"
+BACKUP_FILE="$BACKUP_DIR/Mike_nginx_backup_$DATE.tar.gz"
+LOG_FILE="$BACKUP_DIR/Mike_nginx_backup_log_$DATE.txt"
 
 # Create backup path for Mike Backup if Not present
 
@@ -11,7 +11,7 @@ mkdir -p  $BACKUP_DIR
 
 # Creating backup file  and storing logs in LOG_FILE
 
-tar -czf $BACKUP_FILE /etc/nginx/ /usr/share/nginx/html/ 2 >> $LOG_FILE
+tar -czf $BACKUP_FILE /etc/nginx/ /usr/share/nginx/html/ 2>> $LOG_FILE
 
 # Verification if Backup is get completed
 echo "Backup verification for $BACKUP_FILE on $(date)" >> $LOG_FILE
